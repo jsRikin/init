d8261a48e5c1a7d450de5d145ba161334dbe168e
jsRikin <57054221+jsRikin@users.noreply.github.com>
2019-10-27 11:07:22 +0700

    Add files via upload

---
+++ function.js
@@ -0,0 +1,14 @@
+function hello(){
+    console.log('heelo')
+}
+
+let number = function number1(){
+    return 1;
+}
+
+(function() {
+    var myvar = "local value";
+      console.log(myvar); // undefined
+
+})();
+
