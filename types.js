8aa12124386ee2bf92e5bd357d02c8d122364616
jsRikin <57054221+jsRikin@users.noreply.github.com>
2019-10-27 11:07:01 +0700

    Add files via upload

---
+++ types.js
@@ -0,0 +1,45 @@
+//
+/**
+ * string
+ * number
+ * array
+ * boolean
+ * null
+ * undefine
+ * object
+ */
+
+
+ console.log('hello');
+string
+var name = 'bitto';
+var temp = `ten ${name}`;
+
+console.log(temp);
+number
+
+//- * /
+var number = 10;
+console.log(10 - 1);//9
+console.log(10 - '1');//10-1||9
+console.log(10 + 1);//11
+console.log(10 + '1');//11||101
+console.log(10/2);
+console.log(10*2);
+
+
+console.log(10/'2');
+var a = ['user1', 'user2'];
+
+var user ={ name:'bitto', skill:'js'};
+console.log(user.name)
+ 
+//null undefine
+var temp = null;
+var temp2 = undefined;
+// if(temp === temp2){
+//     console.log('cung gia tri')
+// }
+
+
+console.log(typeof(temp));
---
+++ variable.js
@@ -0,0 +1,18 @@
+let temp = 'let';
+var temp2 = 'var';
+const temp3 = 'const';
+console.log(temp +' '+temp2+' '+ temp3);
+
+console.log(name);//name || error|| bitto
+var  name = ' bitto';
+
+//hoisting js
+var name;
+console.log(name);//name || error|| bitto
+name = ' bitto';
+
+console.log(name);//name || error|| bitto
+let  name = ' bitto';
+const name;
+console.log(name);//name || error|| bitto
+
